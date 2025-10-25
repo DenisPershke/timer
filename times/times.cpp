f1fa90ea83a34e31fa6944ad4f0783a3832fed1a
Denis Pershkin <153399162+DenisPershke@users.noreply.github.com>
2025-10-26 02:59:33 +0300

    Добавьте файлы проекта.

---
+++ times/times.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <chrono>
+#include <thread>
+#include <iomanip>
+
+using namespace std;
+using namespace std::chrono;
+
+int main() {
+   setlocale(LC_ALL, "ru_RU.utf8");
+   while (true) { 
+       auto now = system_clock::now();
+       time_t t = system_clock::to_time_t(now);
+       tm localTime{};
+#ifdef _WIN32
+       localtime_s(&localTime, &t); // для Windows
+#else
+       localtime_r(&t, &localTime); // для Linux/macOS
+#endif
+
+       cout << "\rТекущее время: "
+           << setw(2) << setfill('0') << localTime.tm_hour << ":"
+           << setw(2) << setfill('0') << localTime.tm_min << ":"
+           << setw(2) << setfill('0') << localTime.tm_sec << flush;
+
+       this_thread::sleep_for(1s);
+   }
+}
